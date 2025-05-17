3e3c2636a216fb38466fa72bded13e98793aada4
hasadin.ch <hasadin.ch@inet.co.th>
2025-05-17 15:23:42 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,27 @@
 <script setup>
-import { reactive } from "vue";
-const genderList = ["man", "women", "other"];
-const interestList = ["sports", "music", "art"];
-const formData = reactive({ interests: [] });
-const submitForm = () => {
-  console.log("Form submitted");
-  console.log("formData", formData);
-  console.log("interests", formData.interests);
-};
+import { ref, reactive, watch, watchEffect } from "vue";
+
+const formData = reactive({
+  firstname: "",
+  lastname: "",
+});
+
+const fullname = ref("");
+
+watchEffect(() => {
+    fullname.value = `${formData.firstname} ${formData.lastname}`;
+    console.log('fullname.value : ',fullname.value)
+  }
+);
 </script>
 
 <template>
   <div>
-    <div>
-      <div>Firstname</div>
-      <input type="text" name="firstname" v-model="formData.firstname" />
-    </div>
-    <div>
-      <div>Lastname</div>
-      <input type="text" name="lastname" v-model="formData.lastname" />
-    </div>
-
-    <div>
-      <div>Gender</div>
-      <div v-for="gender in genderList">
-        <input
-          type="radio"
-          name="gender"
-          :value="gender"
-          v-model="formData.gender"
-        />
-        {{ gender }}
-      </div>
-    </div>
-    <div>
-      <div>Interests</div>
-      <div v-for="interest in interestList">
-        <input
-          type="checkbox"
-          name="interest"
-          :value="interest"
-          v-model="formData.interests"
-        />
-        {{ interest }}
-      </div>
-    </div>
-    <div>
-      <div>Description</div>
-      <textarea v-model="formData.description"></textarea>
-    </div>
-    <button @click="submitForm">Submit</button>
+    <div>Fullname : {{ fullname }}</div>
+    <input type="text" v-model="formData.firstname" /> <br />
+    <input type="text" v-model="formData.lastname" />
   </div>
-</template>
+</template> 
 
-<style></style>
+<style scoped>
+</style>
